aefc2efb38ee8b52fc4b0a8c8cf9ddc674a15873
Andra Helton <andrahelton@gmail.com>
2023-01-03 09:19:23 -0700

    Add test for exhibit has attributes

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe Exhibit do
     it 'exists' do
       expect(exhibit).to be_an_instance_of(Exhibit)
     end
-    
+
+    it 'has attributes' do
+      expect(exhibit.name).to eq("Gems and Minerals")
+      expect(exhibit.cost).to eq(0)
+    end
   end
 end
