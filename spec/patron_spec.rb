3a079d24d5496099c364e90c54fab7c4af3c3e74
Andra Helton <andrahelton@gmail.com>
2023-01-03 09:28:54 -0700

    Add test for patron has attributes

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe Patron do
     it 'exists' do
       expect(patron_1).to be_an_instance_of(Patron)
     end
+
+    it 'has attributes' do
+      expect(patron_1.name).to eq("Bob")
+      expect(patron_1.spending_money).to eq(20)
+    end
   end
 
 end
