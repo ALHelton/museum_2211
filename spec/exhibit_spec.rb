1be6621d103d91b7999dd885260a0f65f8b7cfd0
Andra Helton <andrahelton@gmail.com>
2023-01-03 09:25:22 -0700

    Add test for patron exists

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,6 +1,5 @@
 require 'rspec'
 require './lib/exhibit'
-require './lib/patron'
 
 RSpec.describe Exhibit do
   let(:exhibit) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,5 +3,12 @@ require './lib/exhibit'
 require './lib/patron'
 
 RSpec.describe Patron do
-  
+  let(patron_1) { Patron.new("Bob", 20) }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(patron).to be_an_instance_of(Patron)
+    end
+  end
+
 end
